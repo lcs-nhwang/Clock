76ad9838531aa68a3cb82c2bcff83d1d17dd810b
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-06 14:24:29 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Nicholas Hwang on 6/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
