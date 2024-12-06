7d5343bb187171e4b37bf70a325187d18cf0ff5e
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-06 14:29:39 -0500

    Finished Project Organization

---
+++ Clock/Views/LandingView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Nicholas Hwang on 6/12/2024.
+//
+
+import SwiftUI
+struct LandingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LandingView()
+}
